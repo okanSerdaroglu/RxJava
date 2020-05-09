143279ad7aa9e51f8860f762599ad4a3bf68f4fa
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-10 01:40:17 +0300

    observables and observers

--- RxJavaExamples/app/src/main/java/com/example/rxjavaexamples/MainActivity.java
+++ RxJavaExamples/app/src/main/java/com/example/rxjavaexamples/MainActivity.java
@@ -3,12 +3,76 @@ package com.example.rxjavaexamples;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.rxjavaexamples.models.Task;
+import com.example.rxjavaexamples.util.DataSource;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    // ui
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.text);
+
+        Observable<Task> taskObservable = Observable
+                .fromIterable(DataSource.createTasksList()) // observe list
+                .subscribeOn(Schedulers.io()) // work in worker thread
+                .filter(task -> { // filter them in worker
+                    Log.d(TAG, "test: " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return task.isComplete();
+                })
+                .observeOn(AndroidSchedulers.mainThread());
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d(TAG, "onSubscribe: " + Thread.currentThread().getName());
+                Log.d(TAG, "onSubscribe: called");
+            }
+
+            @Override
+            public void onNext(@NonNull Task task) {
+                Log.d(TAG, "onNext: " + Thread.currentThread().getName());
+                Log.d(TAG, "onNext: " + task.getDescription());
+                /**try { blocks main thread
+                 Thread.sleep(1000);
+                 } catch (Exception e) {
+
+                 }*/
+
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.d(TAG, "onError: " + Thread.currentThread().getName());
+                Log.d(TAG, "onError: " + e);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: " + Thread.currentThread().getName());
+                Log.d(TAG, "onComplete: called");
+            }
+        });
     }
 }
