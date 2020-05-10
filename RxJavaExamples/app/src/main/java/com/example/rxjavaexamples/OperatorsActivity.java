d73ea5f3847f6a749b7de43b0c9c4cbaba35da12
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-10 15:26:06 +0300

    operators

---
+++ RxJavaExamples/app/src/main/java/com/example/rxjavaexamples/OperatorsActivity.java
@@ -0,0 +1,321 @@
+package com.example.rxjavaexamples;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.rxjavaexamples.models.Task;
+import com.example.rxjavaexamples.util.DataSource;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.functions.Predicate;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class OperatorsActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    // ui
+    private TextView text;
+
+    private CompositeDisposable disposables = new CompositeDisposable();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operators);
+        text = findViewById(R.id.text);
+        /**overView();*/
+        /**singleObjectCreate();*/
+        listObjectCreate();
+
+    }
+
+    private void repeat (){
+        Observable<Integer> observable = Observable
+                .range(0,9)
+                .subscribeOn(Schedulers.io())
+                .repeat(3)
+                .observeOn(AndroidSchedulers.mainThread());
+
+        observable.subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+    }
+
+    private void map (){
+        Observable<Task> observable = Observable
+                .range(0,9)
+                .subscribeOn(Schedulers.io())
+                .map(new Function<Integer, Task>() {
+                    @Override
+                    public Task apply(Integer integer) throws Throwable {
+                        Log.d(TAG, "apply: "+ Thread.currentThread().getName());
+                        return new Task("this is task with priority: "
+                                + String.valueOf(integer),false,integer);
+                    }
+                }).takeWhile(new Predicate<Task>() {
+                    @Override
+                    public boolean test(Task task) throws Throwable {
+                        return task.getPriority() < 9;
+                    }
+                }).observeOn(AndroidSchedulers.mainThread());
+
+        observable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull Task task) {
+                Log.d(TAG, "onNext: " + task.getPriority());
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+    }
+
+    private void createRange(){
+        Observable<Integer> observable = Observable
+                .range(0,9)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        observable.subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d(TAG, "onSubscribe: ");
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                Log.d(TAG, "onNext: "+integer);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.d(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        });
+
+    }
+
+    /**
+     * two different ways to create an Observable using the Create() operator
+     * Creating an observable from a single object
+     * Creating an observable from a list object
+     */
+
+    private void createSingleObjectWithJust() {
+        final Task task = new Task("Walk", false, 3);
+        Observable<Task> taskObservable = Observable
+                .just(task)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull Task task) {
+                Log.d(TAG, "onNext: " + task.getDescription());
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+
+    private void singleObjectCreate() {
+        final Task task = new Task("Walk", false, 3);
+        Observable<Task> taskObservable = Observable.create(new ObservableOnSubscribe<Task>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Task> emitter) throws Throwable {
+                Log.d(TAG, "subscribe: worked");
+                if (!emitter.isDisposed()) {
+                    emitter.onNext(task);
+                    emitter.onComplete();
+                }
+
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d(TAG, "onSubscribe: ");
+            }
+
+            @Override
+            public void onNext(@NonNull Task task) {
+                Log.d(TAG, "onNext: " + task.getDescription());
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.d(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        });
+    }
+
+    private void listObjectCreate() {
+
+        Observable<Task> taskObservable = Observable.create(new ObservableOnSubscribe<Task>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Task> emitter) throws Throwable {
+                Log.d(TAG, "subscribe: worked");
+                for (Task task : DataSource.createTasksList()) {
+                    if (!emitter.isDisposed()) {
+                        emitter.onNext(task);
+                    }
+                }
+                emitter.onComplete();
+
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d(TAG, "onSubscribe: ");
+            }
+
+            @Override
+            public void onNext(@NonNull Task task) {
+                Log.d(TAG, "onNext: " + task.getDescription());
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.d(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        });
+
+    }
+
+
+    private void overView() {
+        Observable<Task> taskObservable = Observable
+                .fromIterable(DataSource.createTasksList()) // observe list
+                .subscribeOn(Schedulers.io()) // work in worker thread
+                .filter(task -> { // filter them in worker
+                    Log.d(TAG, "test: " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return task.isComplete();
+                })
+                .observeOn(AndroidSchedulers.mainThread());
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d(TAG, "onSubscribe: " + Thread.currentThread().getName());
+                Log.d(TAG, "onSubscribe: called");
+                disposables.add(d);
+            }
+
+            @Override
+            public void onNext(@NonNull Task task) {
+                Log.d(TAG, "onNext: " + Thread.currentThread().getName());
+                Log.d(TAG, "onNext: " + task.getDescription());
+                /**try { blocks main thread
+                 Thread.sleep(1000);
+                 } catch (Exception e) {
+
+                 }*/
+
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.d(TAG, "onError: " + Thread.currentThread().getName());
+                Log.d(TAG, "onError: " + e);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: " + Thread.currentThread().getName());
+                Log.d(TAG, "onComplete: called");
+            }
+        });
+
+
+        disposables.add(taskObservable.subscribe(task -> Log.d(TAG, "accept: ")));
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposables.clear();
+    }
+}
